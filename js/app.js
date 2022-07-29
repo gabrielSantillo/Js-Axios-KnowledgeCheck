89f56f78cfb1a87e957a858f09bb1a0705587b56
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-28 19:38:58 -0600

    Change the place that I created the variable to be inside the function that will request to post in tha API

--- js/app.js
+++ js/app.js
@@ -29,39 +29,45 @@ axios
 */
 
 function post_success(response) {
-    document.body.insertAdjacentHTML(`beforeend`, `
+  document.body.insertAdjacentHTML(
+    `beforeend`,
+    `
     <h1>Posted!</h1>
-    `)
+    `
+  );
 }
 
 function post_failure(error) {
-    document.body.insertAdjacentHTML(`beforeend`, `
+  document.body.insertAdjacentHTML(
+    `beforeend`,
+    `
     <h1>Try again</h1>
-    `)
+    `
+  );
 }
 
 function postUserMessage(details) {
-  axios.request({
-    url: `https://jsonplaceholder.typicode.com/posts`,
-    method: `POST`,
-    data: {
+  let user_title_input = document.getElementById(`title`);
+  let user_title_value = user_title_input[`value`];
+
+  let user_body_input = document.getElementById(`body`);
+  let user_body_value = user_body_input[`value`];
+
+  let user_id_input = document.getElementById(`userId`);
+  let user_id_value = user_id_input[`value`];
+  axios
+    .request({
+      url: `https://jsonplaceholder.typicode.com/posts`,
+      method: `POST`,
+      data: {
         title: `${user_title_value}`,
         body: `${user_body_value}`,
-        userId: `${user_id_value}`
-    }
-  })
-  .then(post_success)
-  .catch(post_failure);
+        userId: `${user_id_value}`,
+      },
+    })
+    .then(post_success)
+    .catch(post_failure);
 }
-
-let user_title_input = document.getElementById(`title`);
-let user_title_value = user_title_input[`value`];
-
-let user_body_input = document.getElementById(`body`);
-let user_body_value = user_body_input[`value`];
-
-let user_id_input = document.getElementById(`userId`);
-let user_id_value = user_id_input[`value`];
 
 let button = document.querySelector(`button`);
 button.addEventListener(`click`, postUserMessage);
